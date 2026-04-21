177943aeb6eecf162e6c8abe86531cd742b890b3
Ardmaf <127151230+Ardmaf@users.noreply.github.com>
2026-04-21 14:34:06 +0100

    app.py

--- app.py
+++ app.py
@@ -256,5 +256,8 @@ def deactivate_worker():
 # =========================
 # RUN APP
 # =========================
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
